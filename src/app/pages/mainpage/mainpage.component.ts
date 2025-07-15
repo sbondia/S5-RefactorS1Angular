d4c722bd83464927ee8d51a1e15a7e095cef095e
Sergi Bondia <sbondia716@gmail.com>
2025-07-15 12:51:09 +0200

    section: contact

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "../../sections/header/header.component";
 import { FeaturesComponent } from "../../sections/features/features.component";
 import { DownloadComponent } from "../../sections/download/download.component";
 import { FaqComponent } from "../../sections/faq/faq.component";
+import { ContactComponent } from '../../sections/contact/contact.component';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [ProductComponent, HeaderComponent, FeaturesComponent, DownloadComponent, FaqComponent],
+  imports: [ProductComponent, HeaderComponent, FeaturesComponent, DownloadComponent, FaqComponent, ContactComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })

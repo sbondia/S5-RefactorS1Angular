421f1d76df02f2f541494338f0ace775f0937a73
Sergi Bondia <sbondia716@gmail.com>
2025-07-15 10:39:03 +0200

    section: faq

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -3,10 +3,11 @@ import { ProductComponent } from '../../sections/product/product.component';
 import { HeaderComponent } from "../../sections/header/header.component";
 import { FeaturesComponent } from "../../sections/features/features.component";
 import { DownloadComponent } from "../../sections/download/download.component";
+import { FaqComponent } from "../../sections/faq/faq.component";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [ProductComponent, HeaderComponent, FeaturesComponent, DownloadComponent],
+  imports: [ProductComponent, HeaderComponent, FeaturesComponent, DownloadComponent, FaqComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })

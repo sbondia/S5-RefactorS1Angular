7f81012fbb764b6e85565b5a06c6492af1bbca9c
Sergi Bondia <sbondia716@gmail.com>
2025-07-15 06:01:43 +0200

    section: header

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from '../../sections/product/product.component';
+import { HeaderComponent } from "../../sections/header/header.component";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })

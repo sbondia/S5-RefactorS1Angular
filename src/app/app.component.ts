a77a3e5d12c9f0f744e64e2db93e7ae943f26ff2
Sergi Bondia <sbondia716@gmail.com>
2025-07-19 10:06:21 +0200

    header/footer extraction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainpageComponent } from './pages/mainpage/mainpage.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { FooterComponent } from './sections/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainpageComponent],
+  imports: [RouterOutlet, MainpageComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductComponent } from '../../sections/product/product.component';
-import { HeaderComponent } from "../../sections/header/header.component";
 import { FeaturesComponent } from "../../sections/features/features.component";
 import { DownloadComponent } from "../../sections/download/download.component";
 import { FaqComponent } from "../../sections/faq/faq.component";
 import { ContactComponent } from '../../sections/contact/contact.component';
-import { FooterComponent } from "../../sections/footer/footer.component";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [ProductComponent, HeaderComponent, FeaturesComponent, DownloadComponent, FaqComponent, ContactComponent, FooterComponent],
+  imports: [ProductComponent, FeaturesComponent, DownloadComponent, FaqComponent, ContactComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
